cc63e7ebff73ae4395881769b1fdcd52c61cce98
Bernard Strong <bstrong71@gmail.com>
2017-08-12 11:50:52 -0400

    search request to API returns expected data

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,34 +1,46 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
 
 // 1. First select and store the elements you'll be working with
 
+let musicPlayer = document.getElementById('musicPlayer');
+let button = document.getElementById('button');
+let resultsSection = document.getElementById('resultsSection');
 
 
-// 2. Create your `submit` event for getting the user's search term
 
+//********* Search Button Event *********************************//
+button.addEventListener("click", function(){
+  let search = document.getElementById('search');
+  console.log(search.value);
+//This adds the required "+" to the string for iTunes//
+  let str = search.value.split(' ').join('+');
+  console.log(str);
+//Makes entry equal to the API string and limits results to 25//
+  let entry = "https://itunes.apple.com/search?term=" + str + "&limit=25";
+//Clears out results section in case of previous searches//
+  resultsSection.innerHTML = "";
+//Boilerplate fetch from API//
+  fetch(entry)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          console.log("Here is the data:", data);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
+
+
+
+})
 
 
 // 3. Create your `fetch` request that is called after a submission
-fetch("https://itunes.apple.com/search?term=jack+johnson&limit=25")
-  // Data is fetched and we get a promise.
-  .then(
-    // The promise returns a response from the server.
-    function(response) {
-      // We process the response accordingly.
-      if (response.status !== 200) {
-        console.log(response.status);
-        return;
-      }
-      response.json().then(function(data) {
-        console.log("Here is the data:", data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log("Fetch Error :-S", err);
-  });
 
 
 // 4. Create a way to append the fetch results to your page

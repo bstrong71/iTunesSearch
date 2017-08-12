ababca0799e2922c04b510fb33aeb1d0adc4e9d3
Bernard Strong <bstrong71@gmail.com>
2017-08-12 13:41:26 -0400

    pushing data in format to search results section

--- starter_files/main.js
+++ starter_files/main.js
@@ -10,13 +10,14 @@ let resultsSection = document.getElementById('resultsSection');
 //********* Search Button Event *********************************//
 button.addEventListener("click", function(){
   let search = document.getElementById('search');
-  console.log(search.value);
+  let song = musicPlayer.src;
+  console.log(musicPlayer.src);
 //This adds the required "+" to the string for iTunes//
   let str = search.value.split(' ').join('+');
   console.log(str);
 //Makes entry equal to the API string and limits results to 25//
-  let entry = "https://itunes.apple.com/search?term=" + str + "&limit=25";
-//Clears out results section in case of previous searches//
+  let entry = "https://itunes.apple.com/search?term=" + str + "&limit=20";
+// Clears out results section in case of previous searches//
   resultsSection.innerHTML = "";
 //Boilerplate fetch from API//
   fetch(entry)
@@ -27,16 +28,32 @@ button.addEventListener("click", function(){
           return;
         }
         response.json().then(function(data) {
-          console.log("Here is the data:", data);
+          console.log("Here is the data: ", data);
+          console.log(data.results.length);
+          for (var i = 0; i < data.results.length; i++) {
+
+            let songResult = document.createElement('article');
+            songResult.setAttribute("class", "each_container");
+
+            let songDetails = document.createElement('div');
+            songDetails.setAttribute('class', 'song_details');
+            songDetails.innerHTML =
+            `
+              <img src=${data.results[i].artworkUrl100}>
+              <p>${data.results[i].trackName}</p>
+              <p>${data.results[i].artistName}</p>
+            `
+
+            songResult.appendChild(songDetails);
+            resultsSection.appendChild(songResult);
+          }
+
         });
       }
     )
     .catch(function(err) {
       console.log("Fetch Error :-S", err);
     });
-
-
-
 })
 
 
